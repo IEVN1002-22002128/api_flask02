8573514c250be8206ae8954e281f4ed6af90addf
Danae <danaelopezdleon@gmail.com>
2025-11-05 20:06:18 -0600

    calcular distancia

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import forms
+import math
+
+class CalculadoraDistancia:
+    def __init__(self, xa=0, ya=0, xb=0, yb=0):
+        self.coordXA = xa
+        self.coordYA = ya
+        self.coordXB = xb
+        self.coordYB = yb
+        self.distanciaCalculada = 0
+
+    def ejecutar_calculo(self):
+        self.distanciaCalculada = math.sqrt(
+            (self.coordXB - self.coordXA) ** 2 +
+            (self.coordYB - self.coordYA) ** 2
+        )
+
+    def mostrar_resultado(self):
+        return f"Distancia calculada: {self.distanciaCalculada:.2f} unidades"
+
 
 app = Flask(__name__)
 
@@ -8,13 +28,71 @@ def index():
   listado=['Python', 'Flask', 'Jinja2', 'HTML', 'CSS']
   return render_template('index.html', titulo=titulo, listado=listado)
 
-@app.route('/calculos')
-def about():
-  return render_template('calculos.html')
+@app.route('/calculos', methods=['GET', 'POST'])
+def calculos():
 
-@app.route('/distancia')
+    if request.method == 'POST':
+        numero1 = request.form.get('numero1', '')
+        numero2 = request.form.get('numero2', '')
+        operacion = request.form.get('operacion', '')
+
+        try:
+            n1, n2 = int(numero1), int(numero2)
+            if operacion == 'suma':
+                resultado = n1 + n2
+            elif operacion == 'resta':
+                resultado = n1 - n2
+            elif operacion == 'multiplicacion':
+                resultado = n1 * n2
+            elif operacion == 'division':
+                resultado = n1 / n2 if n2 != 0 else "Div entre cero"
+        except ValueError:
+            resultado = "Error: Ingresa números válidos"
+
+    return render_template('calculos.html',
+                           resultado=resultado,
+                           numero1=numero1,
+                           numero2=numero2)
+
+@app.route('/distancia', methods=['GET', 'POST'])
 def distancia():
-  return render_template('distancia.html')
+    resultado = None
+    puntoA_X = puntoA_Y = puntoB_X = puntoB_Y = ''
+
+    if request.method == 'POST':
+        try:
+            puntoA_X = float(request.form.get('puntoA_X', 0))
+            puntoA_Y = float(request.form.get('puntoA_Y', 0))
+            puntoB_X = float(request.form.get('puntoB_X', 0))
+            puntoB_Y = float(request.form.get('puntoB_Y', 0))
+
+            calc = CalculadoraDistancia(puntoA_X, puntoA_Y, puntoB_X, puntoB_Y)
+            calc.ejecutar_calculo()
+            resultado = calc.mostrar_resultado()
+
+        except ValueError:
+            resultado = "Error: Ingresa valores numéricos válidos"
+
+    return render_template('distancia.html',
+                           resultado=resultado,
+                           puntoA_X=puntoA_X,
+                           puntoA_Y=puntoA_Y,
+                           puntoB_X=puntoB_X,
+                           puntoB_Y=puntoB_Y)
+
+@app.route("/Alumnos",methods=['GET','POST'])
+def alumnos():
+  mat=0
+  nom=""
+  ape=""
+  email=""
+  alumno_clas=forms.UserForm(request.form)
+  if request.method == 'POST' and alumno_clas.validate():
+    mat=alumno_clas.matricula.data
+    nom=alumno_clas.nombre.data
+    ape=alumno_clas.apellido.data
+    email=alumno_clas.correo.data
+  return render_template('alumnos.html',form=alumno_clas, mat=mat,nom=nom,ape=ape,email=email)
 
 @app.route('/user/<string:user>')
 def user(user):
@@ -60,6 +138,3 @@ def func3():
 
 if __name__ == '__main__':
   app.run(debug=True)
-
-
-
---
+++ forms.py
@@ -0,0 +1,17 @@
+from wtforms import Form
+from wtforms import StringField, PasswordField, EmailField, BooleanField, SubmitField, IntegerField
+from wtforms import validators
+
+class UserForm(Form):
+    matricula=IntegerField("Matricula",[
+        validators.DataRequired(message='El campo es requerido')
+    ])
+    nombre=StringField("Nombre",[
+        validators.DataRequired(message='El campo es requerido')
+    ])
+    apellido=StringField("Apellido",[
+        validators.DataRequired(message='El campo es requerido')
+    ])
+    correo=StringField("Correo",[
+        validators.DataRequired(message='El campo es requerido')
+    ])
